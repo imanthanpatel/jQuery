f741d2bd37a33703b371d128c66bf57d2a5ba50b
Manthan Patel <manthan002408@gmail.com>
2025-05-06 15:04:13 +0530

    i

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log($)
+
+$document.ready(function(){ //when document is ready code wil start working
+    //all code
+    console.log(jQuery);
+// $('select0r').action() //syntex
+// $('p').click();
+$('p').click(function(){
+
+console.log('you clicked ');
+// $('#id').hide();
+// $('.class').hide();
+
+
+
+});// do this on click on p tag
+
+
+});
+// there are 3 main type of selector in jQuery
+// 1. Element selector
+// 2. Class selector   
+// 3. ID selector
